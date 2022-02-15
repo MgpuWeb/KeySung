772e86748404b37cc5eef03e5b4cd8021260f380
n0tm <a.kutin@affise.com>
2022-02-15 20:03:20 +0300

    добавил options запросы

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ return [
     'bsVersion' => '4.x',
     'services' => [
     	'emotions' => [
-    		'base_url'   => 'http://37.230.157.130:7677',
+    		'base_url'   => 'http://172.22.26.105:7677',
 			'images_url' => 'http://172.22.26.105:8080'
 		]
 	],
